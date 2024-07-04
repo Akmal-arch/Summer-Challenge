fd0fc0668fb8705878013f877d263c1e78e91fc9
Akmal <akmal9999007@gmail.com>
2024-07-04 14:12:24 +0500

    day-4 done

---
+++ day-4/app.js
@@ -0,0 +1,39 @@
+document.querySelectorAll(".dock img").forEach(img => {
+    img.addEventListener("click", e => {
+        e.currentTarget.classList.add("loading");
+
+    });
+
+    img.addEventListener("mousemove", e => {
+        let item = e.target;
+        let itemRect = item.getBoundingClientRect();
+        let offset = Math.abs(e.clientX - itemRect.left) / itemRect.width;
+
+        let prev = item.previousElementSibling || null;
+        let next = item.nextElementSibling || null;
+
+        let scale = 0.6;
+
+        resetScale();
+
+        if(prev) {
+            prev.style.setProperty("--scale", 1 + scale * Math.abs(offset -1));
+        }
+
+        item.style.setProperty('--scale', 1 + scale);
+
+        if(next) {
+            next.style.setProperty("--scale", 1 + scale * offset);
+        }
+    });
+});
+
+document.querySelector(".dock").addEventListener("mouseleave", e => {
+    resetScale();
+})
+
+function resetScale() {
+    document.querySelectorAll(".dock img").forEach(img => {
+        img.style.setProperty("--scale", 1)
+    })
+}
